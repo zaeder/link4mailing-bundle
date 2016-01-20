2c21d5809e0de7649a7646af3ed97756a1859c1b
Florian THIERRY <zaeder@live.fr>
2016-01-20 11:02:31 +0100

    Init project

---
+++ ZaederLink4MailingBundle.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author: Florian THIERRY <zaeder@live.fr>
+ */
+
+namespace Zaeder\Link4MailingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZaederLink4MailingBundle extends Bundle
+{
+}
