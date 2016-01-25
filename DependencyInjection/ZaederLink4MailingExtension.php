2b7384673d1692157a4bcdfd7a0e6206bc7db683
Florian THIERRY <zaeder@live.fr>
2016-01-25 19:06:46 +0100

    Update Extension

--- DependencyInjection/ZaederLink4MailingExtension.php
+++ DependencyInjection/ZaederLink4MailingExtension.php
@@ -2,56 +2,26 @@
 
 namespace Zaeder\Link4MailingBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class ZaederLink4MailingExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $processor = new Processor();
         $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
 
-        $config = $processor->processConfiguration($configuration, $configs);
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 
-        $this->remapParametersNamespaces($config, $container, array(
-            '' => array(
-                'firewall_name' => 'zaeder_link4mailing.firewall_name',
-                'user_class' => 'zaeder_link4mailing.model.user.class',
-            ),
-        ));
-    }
-
-    protected function remapParameters(array $config, ContainerBuilder $container, array $map)
-    {
-        foreach ($map as $name => $paramName) {
-            if (array_key_exists($name, $config)) {
-                $container->setParameter($paramName, $config[$name]);
-            }
-        }
-    }
-
-    protected function remapParametersNamespaces(array $config, ContainerBuilder $container, array $namespaces)
-    {
-        foreach ($namespaces as $ns => $map) {
-            if ($ns) {
-                if (!array_key_exists($ns, $config)) {
-                    continue;
-                }
-                $namespaceConfig = $config[$ns];
-            } else {
-                $namespaceConfig = $config;
-            }
-            if (is_array($map)) {
-                $this->remapParameters($namespaceConfig, $container, $map);
-            } else {
-                foreach ($namespaceConfig as $name => $value) {
-                    $container->setParameter(sprintf($map, $name), $value);
-                }
-            }
+        foreach (array('user_class','firewall_name') as $attribute) {
+            $container->setParameter('zaederlink4mailingbundle.'.$attribute, $config[$attribute]);
         }
     }
 }
