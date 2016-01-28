788f6fab858017d37e2104676d8c8d5281ad8f3e
Florian THIERRY <zaeder@live.fr>
2016-01-28 14:12:44 +0100

    generate link and redirect

--- Controller/Link4mailingController.php
+++ Controller/Link4mailingController.php
@@ -3,17 +3,13 @@
 namespace Zaeder\Link4mailingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class Link4mailingController extends Controller
 {
     public function Link4mailingAction($linkId, $securKey)
     {
-        $request = $this->get('request');
-        $url = 'http://'.$request->getHost().$request->getBaseUrl().'/';
-
-    	$response = new RedirectResponse($url);
+    	$response = new RedirectResponse($this->get('Link4mailing_urlhelper')->getLink($linkId, $securKey));
     	
     	return $response;
     }   
--- Service/UrlHelper.php
+++ Service/UrlHelper.php
@@ -4,38 +4,108 @@ namespace Zaeder\Link4mailingBundle\Service;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\ORM\EntityManager;
+use Zaeder\Link4mailingBundle\Entity\Link4mailing;
 
 class UrlHelper {
     
+    /**
+     * Container
+     * @var Symfony\Component\DependencyInjection\ContainerInterface 
+     */
     private $container;
+    /**
+     * Entity Manager
+     * @var Doctrine\ORM\EntityManager
+     */
     private $em;
+    /**
+     * User repository
+     * @var object
+     */
     private $userRepository;
+    /**
+     * Link4mailing repository
+     * @var Zaeder\Link4mailingBundle\Entity\Link4mailing
+     */
     private $link4mailingRepository;
     
+    /**
+     * Init vars
+     * @param ContainerInterface $container
+     * @param EntityManager $entity_manager
+     */
     function __construct(ContainerInterface $container, EntityManager $entity_manager)
     {
         $this->container = $container;
         $this->em = $entity_manager;
-        $this->getUserRepository();
+        $this->userRepository = $this->em->getRepository($this->container->getParameter('zaederlink4mailingbundle.user_class'));
         $this->link4mailingRepository = $this->em->getRepository('ZaederLink4mailingBundle:Link4mailing');
     }
     
-    private function getUserRepository()
+    /**
+     * Generate link
+     * @param string $routeName
+     * @param mixed $routeParams
+     * @param int $userId
+     * @param int $duration
+     */
+    public function generateLink($routeName, $routeParams = array(), $userId = 0, $duration = 0)
     {
-        $userEntity = $this->container->getParameter('zaederlink4mailingbundle.user_class');
-        $userEntityArr = explode('\\', $userEntity);
-        $bundleName = $entityName = '';
-        for($loop = 0; $loop < count($userEntityArr); $loop++){
-            if(isset($userEntityArr[$loop]) && $userEntityArr[$loop] !== '' && isset($userEntityArr[$loop-1]) && $userEntityArr[$loop-1] === 'Entity'){
-                $entityName .= $userEntityArr[$loop];
-            } elseif(isset($userEntityArr[$loop]) && $userEntityArr[$loop] !== '' && $userEntityArr[$loop] !== 'Entity'){
-                $bundleName .= $userEntityArr[$loop];
-            }
-        }
-        if($bundleName !== '' && $entityName !== ''){
-            $this->userRepository = $this->em->getRepository($bundleName . ':' . $entityName);
+        $link4mailing = new Link4mailing();
+        if(is_array($routeParams)){
+            $this->container->get('router')->generate($routeName, $routeParams);
         } else {
-            throw new \LogicException('Can\'t find the repository');
+            throw new \LogicException('$routeName must be an array');
         }
+        if(!is_int($userId)) {
+            throw new \LogicException('$userId must be an integer');
+        }
+        if(!is_int($duration)) {
+            throw new \LogicException('$duration must be an integer');
+        } elseif($duration > 0){
+            $time = strtotime('+' . $duration . ' hour');
+            $expirationDate = new \DateTime();
+            $expirationDate->setTimestamp($time);
+            $link4mailing->setExpirationDate($expirationDate);
+        }
+        $token = md5(substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!:;,§/.?*ùµ%|_-#=()'), 0, 30));
+        $link4mailing->setRouteName($routeName)
+                     ->setRouteParams(json_encode($routeParams))
+                     ->setToken($token)
+                     ->setUserId($userId)
+                     ->setIsActive(1);
+        $this->em->persist($link4mailing);
+        $this->em->flush();
+        return $this->container->get('router')->generate('zaeder.link4mailing', array(
+            'linkId' => $link4mailing->getId(),
+            'securKey' => md5($routeName . $token . json_encode($routeParams))
+        ));
+    }
+    
+    public function getLink($linkId, $securKey)
+    {
+        $link4mailing = $this->link4mailingRepository->find($linkId);
+        if($link4mailing instanceof Link4mailing && $link4mailing->getIsActive() === true){
+            $date = new \DateTime();
+            $expirationDate = $link4mailing->getExpirationDate();
+            if(!is_null($expirationDate) && $expirationDate < $date){
+                throw new \Exception('This link has expired');
+            }
+            if($securKey === md5($link4mailing->getRouteName() . $link4mailing->getToken() . $link4mailing->getRouteParams())){
+                if($link4mailing->getUserId() !== 0){
+                    $this->autologin($link4mailing->getUserId());
+                }
+                return $this->container->get('router')->generate($link4mailing->getRouteName(), json_decode($link4mailing->getRouteParams()));
+            } else {
+                throw new \Exception('This link is not valid');
+            }
+        } else {
+            throw new \Exception('Not found link');
+        }
+    }
+    
+    private function autologin($userId)
+    {
+        $user = $this->userRepository->find($userId);
     }
 }
